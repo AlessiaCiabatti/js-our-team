fcef7c9d6fceebd6c7219996fbd6198be4d55c7c
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-05 16:47:51 +0100

    [add] js-img

--- js/script.js
+++ js/script.js
@@ -10,36 +10,36 @@ const teamMembers = [
     role: 'Founder & CEO',
     // 3
     // img
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'our-team/img/wayne-barnett-founder-ceo.jpg',
   },
 
   {
     fullName: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'our-team/img/angela-caroll-chief-editor.jpg',
   },
 
   {
     fullName: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'our-team/img/walter-gordon-office-manager.jpg',
   },
 
   {
     fullName: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'our-team/img/angela-lopez-social-media-manager.jpg',
   },
 
   {
     fullName: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'our-team/img/scott-estrada-developer.jpg',
   },
   
   {
     fullName: 'Barbara Ramos',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'our-team/img/barbara-ramos-graphic-designer.jpg',
   },
 ]
 
@@ -54,6 +54,7 @@ for (let member of teamMembers){
     <div class="card" style="width: 18rem;">
       
       <div class="card-body">
+        <img src="${member.image}" class="card-img-top" alt="...">
         <h5 class="card-title">${member.fullName}</h5>
         <p class="card-text">${member.role}</p>
       </div>
