998493e5f8fd335922c1e0040e8937171873c0cb
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-03-05 15:55:56 +0100

    [add] html/card-js/output

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// 2
+// richiamare output
+const output = document.querySelector('.output');
+
+// 1
+// Creare l’array di oggetti con le informazioni fornite
+lista
 const teamMembers = [
   {
     fullName: 'Wayne Barnett',
